adf5deb47f64bb0cddc53d1fc70afb0616762d99
Bazzak <bazzak81@gmail.com>
2019-01-09 12:31:53 +0100

    API changes

--- WeatherLampAPI/src/main/java/Main.java
+++ WeatherLampAPI/src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> {
             Connection connection = new Connection("Lomma", 0, 0);
+            new GetWeather().getValues(connection.getWeather());
             return connection.getWeather();
 
         });
