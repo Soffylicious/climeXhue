1bd90dd7d01967850c015c702ff77c3c983171b0
Bazzak <bazzak81@gmail.com>
2019-01-10 14:06:09 +0100

    API weather complete

--- climeXhueAPI/src/main/java/GetWeather.java
+++ climeXhueAPI/src/main/java/GetWeather.java
@@ -1,17 +1,35 @@
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GetWeather {
 
-    public GetWeather() {}
+    public GetWeather() {
+    }
 
-    public String getValues(String weatherAnswer) throws ParseException {
-        JSONParser parser = new JSONParser();
-        JSONObject o = (JSONObject) parser.parse(weatherAnswer);
-        System.out.println(o);
+    //TODO weater:main, weather:description, weather:icon (d or n), main:humidity, main:temp, wind:speed
 
 
-        return null;
+    public JSONObject getValues(String weatherAnswer) {
+        JSONObject o = new JSONObject(weatherAnswer);
+
+        JSONArray array = o.getJSONArray("weather");
+        JSONObject weather = array.getJSONObject(0);
+        String mainWeather = weather.getString("main");
+        String details = weather.getString("description");
+        boolean day = weather.getString("icon").contains("d");
+
+        JSONObject main = o.getJSONObject("main");
+        int temp = (int) main.getDouble("temp");
+        int humidity = main.getInt("humidity");
+
+        JSONObject wind = o.getJSONObject("wind");
+        int speed = (int) wind.getDouble("speed");
+        String cityName = o.getString("name");
+
+        JSONObject reply = new JSONObject();
+        reply.put("cityName", cityName).put("mainWeather", mainWeather).put("details", details).put("day", day)
+                .put("temperature", temp).put("humidity", humidity).put("windSpeed", speed);
+
+        return reply;
     }
 }
--- climeXhueAPI/src/main/java/Main.java
+++ climeXhueAPI/src/main/java/Main.java
@@ -5,10 +5,13 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> {
-            Connection connection = new Connection("Lomma", 0, 0);
-            new GetWeather().getValues(connection.getWeather());
-            return connection.getWeather();
+
+        get("/city/:city", (req, res) -> {
+            Connection connection = new Connection(req.params(":city"), 0, 0);
+            System.out.println("line 10");
+            String answer = connection.getWeather();
+            System.out.println("line 12");
+            return new GetWeather().getValues(answer);
 
         });
     }
