70a90efe26b830d7213ad9f2553ebc286a3ab2c6
Bazzak <bazzak81@gmail.com>
2019-01-12 16:47:24 +0100

    API comunicate with MQTT

--- climeXhueAPI/src/main/java/ParseJSON.java
+++ climeXhueAPI/src/main/java/ParseJSON.java
@@ -1,15 +1,20 @@
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class GetWeather {
+import java.net.URISyntaxException;
 
-    public GetWeather() {
+public class ParseJSON {
+
+    private MqttConnection mqttConnection = new MqttConnection("tcp://m20.cloudmqtt.com:14778");
+
+    public ParseJSON() throws MqttException, URISyntaxException {
     }
 
     //TODO weater:main, weather:description, weather:icon (d or n), main:humidity, main:temp, wind:speed
 
 
-    public JSONObject getValues(String weatherAnswer) {
+    public JSONObject getValues(String weatherAnswer) throws MqttException, URISyntaxException {
         JSONObject o = new JSONObject(weatherAnswer);
 
         JSONArray array = o.getJSONArray("weather");
@@ -26,9 +31,23 @@ public class GetWeather {
         int speed = (int) wind.getDouble("speed");
         String cityName = o.getString("name");
 
+        String color = "";
+        if (mainWeather.equals("Thunderstorm")) color = "thunderstorm";
+        else if (mainWeather.equals("Drizzle")) color = "drizzle";
+        else if (mainWeather.equals("Rain")) color = "rain";
+        else if (mainWeather.equals("Snow")) color = "snow";
+        else if (mainWeather.equals("Atmosphere")) color = "atmosphere";
+        else if (mainWeather.equals("Clear")) color = "clear";
+        else if (mainWeather.equals("Clouds")) color = "clouds";
+
+
         JSONObject reply = new JSONObject();
         reply.put("cityName", cityName).put("mainWeather", mainWeather).put("details", details).put("day", day)
                 .put("temperature", temp).put("humidity", humidity).put("windSpeed", speed);
+
+        System.out.println(mainWeather);
+
+        mqttConnection.sendMessage("1,hue," + color);
 
         return reply;
     }
